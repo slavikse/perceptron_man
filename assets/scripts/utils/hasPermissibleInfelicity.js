0c483665356f1b51f36d7ea03f12e967613a6905
slavikse <gslavikse@gmail.com>
2019-09-05 17:55:03 +0300

    Уменьшение допустимой погрешности.

--- assets/scripts/utils/hasPermissibleInfelicity.js
+++ assets/scripts/utils/hasPermissibleInfelicity.js
@@ -1,6 +1,6 @@
 // Диапазон ускорения в пределах погрешности при столновении с объектом.
 module.exports = (value) => {
   // Допустимая погрешность эмуляции физических тел.
-  const infelicity = 3;
-  return value > -infelicity && value < infelicity;
+  const infelicity = 1;
+  return -infelicity <= value && value <= infelicity;
 };
