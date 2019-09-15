df27f0c7da904f9c01d40f869b5945675d40a3be
slavikse <gslavikse@gmail.com>
2019-09-16 00:06:57 +0300

    Обновлено описание планов по проекту.

--- assets/scripts/character/character.js
+++
@@ -1,8 +0,0 @@
-cc.Class({
-  extends: cc.Component,
-
-  onLoad() {
-    const physicsManager = cc.director.getPhysicsManager();
-    physicsManager.enabled = true;
-  },
-});
---
+++ assets/scripts/perceptron/perceptron.js
@@ -0,0 +1,9 @@
+cc.Class({
+  extends: cc.Component,
+
+  onLoad() {
+    // todo
+    // const physicsManager = cc.director.getPhysicsManager();
+    // physicsManager.enabled = true;
+  },
+});
